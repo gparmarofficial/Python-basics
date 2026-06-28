c8a15dc3ad4e671a5c3908ad6e4a2044deb57208
gparmarofficial <gparmar.official@gmail.com>
2026-06-28 13:44:26 +0200

    Add files via upload

---
+++ routine.py
@@ -0,0 +1,32 @@
+#step 1 i wake up
+print("i wake up at 6")
+
+#step 2 getting ready
+print("i get ready for school")
+
+#step 3 going to school
+print("i take the bus at 7 am")
+      
+#step 4 in school
+print ("i reach school at 7.30 am and till 13.15 i leave")
+
+#step 5 home
+print ("i arrive at 14.pm and take a shower then eat lunch")
+
+#step 6 tests hmrk 
+print("if i have any tests or homework i revise or do em if not i just go out with my friends or watch tv")
+
+#step 7 lil bro
+print("at 4pm i pick up my little brother from his Elemantaty school and we both come home")
+
+#step 8 chores
+print("at that time my mum has arrived home as well so i help around the house")
+
+#step 9 dinner
+print("at dinner my dad arrives so we all eat together and afterwards watch a movie")
+
+#step 10 sleep
+print("i go to bed around 10.30pm")
+
+#step 11 calculate total study time
+print("TOtal study hours in a week:", 1.5*5)
