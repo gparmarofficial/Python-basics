a70790acd385854b5b5068f996ed4f012a5b9e1d
gparmarofficial <gparmar.official@gmail.com>
2026-07-02 13:39:26 +0200

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,20 @@
+#1
+x = 10 
+print(type(x))
+
+
+#activity2
+x = 10 
+
+y = float(x)#
+print(y)
+print(type(y))
+
+#activity3
+ #define the original string
+word="CODINGAL"
+
+#reverse the string using slicing
+reversed_word = word[::-1]
+#print the result
+print(reversed_word)
