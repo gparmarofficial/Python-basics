d25993c72c9c127104692881df8c6480f6ba0f1e
gparmarofficial <gparmar.official@gmail.com>
2026-07-03 13:07:38 +0200

    Add files via upload

---
+++ Lesson4.py
@@ -0,0 +1,27 @@
+#activity1
+t1= 98
+t2= 94
+t3=41
+t4=96
+t5=11
+
+#add all the variables together
+total_height= t1+t2+t3+t4+t5
+
+# divide by the total numberof trees
+average_height = total_height /5 
+# print the result
+print(average_height)
+
+#actvity3
+maths = 40 
+science = 70
+hindi = 50
+english = 60 
+
+#add al the scores to get the total scored marks
+total_scored = maths + science + hindi + english
+# calculate the 
+percentage = (total_scored / 400)*100
+#print the result
+print("rajs total percantage is:",percentage)
