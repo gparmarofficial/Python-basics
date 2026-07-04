ed030464f76eca84df1d1ad96eb9d18698f41e78
gparmarofficial <gparmar.official@gmail.com>
2026-07-04 23:12:09 +0200

    Add files via upload

---
+++ hmrk uppercase.py
@@ -0,0 +1,8 @@
+# Input the word in lowercase
+word = input("Enter the word in lowercase: ")
+
+# Convert to uppercase
+upper_word = word.upper()
+
+# Display the result
+print("Word in uppercase:", upper_word)
