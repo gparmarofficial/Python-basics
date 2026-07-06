577607496949c8e8d070a8db29cc36dc9f04fb01
gparmarofficial <gparmar.official@gmail.com>
2026-07-06 11:16:57 +0200

    Add files via upload

---
+++ classwork.py
@@ -0,0 +1,52 @@
+# Program to check the application of logical not operator
+
+a = 10
+b = 12
+c = 12
+
+# not is used here to reverse the result of (a == b)
+print(not (a == b))
+
+# not is used here to reverse the result of (b == c)
+print(not (b == c))
+
+a = "python"
+b = "coding"
+
+# not is used here to check that a is not equal to b
+if not (a == b):
+    print(a, 'and', b, 'are different.')
+
+a = 4
+b = 5
+
+# not is used here to reverse the result of comparing both conditions
+if not ((a == 1) == (b == 5)):
+    print('Hello')
+
+a = int(input("Enter a number: "))
+
+# not is used here to check that the number is not even
+if not (a % 2 == 0):
+    print(a, "is an odd number.")
+
+#activity3
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+
+BMI = weight / (height/100)**2
+
+print("Your BMI is", BMI)
+
+if BMI <= 18.4:
+    print("You are underweight.")
+elif BMI <= 24.9:
+    print("You are healthy.")
+elif BMI <= 29.9:
+    print("You are over weight.")
+elif BMI <= 34.9:
+    print("You are severely over weight.")
+elif BMI <= 39.9:
+    print("You are obese.")
+else:
+    print("You are severely obese.")
