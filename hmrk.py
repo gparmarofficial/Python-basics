ca874c2efd60b0feb4a04ee6a3d0ffffac9e18b2
gparmarofficial <gparmar.official@gmail.com>
2026-07-08 11:26:51 +0200

    Add files via upload

---
+++ hmrk.py
@@ -0,0 +1,6 @@
+temperature = float(input("Enter the current temperature:"))
+if temperature >= 22:
+    print("its warm u can wear light clothes")
+    
+else:
+    print("it is cold pls wear ur jacket")
