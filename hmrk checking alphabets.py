d736d4b49636b77b1861f4e4cb9ee44f22b74931
gparmarofficial <gparmar.official@gmail.com>
2026-07-10 11:24:38 +0200

    Add files via upload

--- hmrk checking alphabets.py
+++ hmrk checking alphabets.py
@@ -1,6 +1,26 @@
-charn = input ("Enter a character :")
+ch = input("Enter a character: ")
 
-if a <= char <= z or A <= char <= Z:
-    print(char, "is an alphabet")
+if ch.isalpha():
+    print("Alphabet")
 else:
-    print(char, "is not an alphabet")
+    print("Not Alphabet")
+    
+#hmrkcheckage
+age = int(input())
+
+if age >= 10 and age <= 20:
+    print("Between 10 and 20")
+else:
+    print("Not between 10 and 20")
+    
+#hmrkpowercalculater
+
+num = int(input("Enter number: "))
+n = int(input("Enter power: "))
+
+result = 1
+
+for i in range(n):
+    result = result * num
+
+print("Answer =", result)
