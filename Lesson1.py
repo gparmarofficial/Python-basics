045f20ce46470f38b051f8282115f1eec6d09034
gparmarofficial <gparmar.official@gmail.com>
2026-07-14 13:06:32 +0200

    Add files via upload

--- Lesson1.py
+++ Lesson1.py
@@ -1,47 +1,34 @@
-medical = input("Medical cause?(Y/N):").upper()
-if medical =="Y":
-    print("allowed")
-else:
-    attendance = int(input("Enter attendance % :"))
-    if attendance > 75:
-        print("allowed")
-    else:
-        print("not allowed")
-        
+def wishes():
+    print("hello, how are u!")
 
-#activity2
-units = float(input("Enter units consumed:"))
-if units < 50:
-    rate = 2.60
-    tax = 25
-elif units >50 and units <100:
-    rate = 3.25
-    tax = 35
-elif units > 100 and units <200:
-    rate = 5.26
-    tax = 45
-else:
-    rate = 8.45
-    tax = 75
+wishes()
 
-total_bill = (units * rate) + tax
+# activity2
+def weather():
+    print("autum:cool and windy")
+    print("spring: warm and sunny")
+    
+weather()
 
-print ("Total bill:",total_bill)
+# activity3
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def mul(a,b):
+    print(a*b)
+def div(a,b):
+    print(a/b)
 
-#activity3
-type_choise = input("Enter 1 for Bike or 2 for car:")
-if type_choise == "1":
-    bike_choise = input("Enter 1 for scooter 2 for sports bike:")
-    if bike_choise =="1":
-        print("you chose a scooter!")
-    else:
-        print("you can chose a sports bike")
-elif  type_choise == "2":
-    car_choise = input ("Enter 1 for Sedan and 2 for the SUV")
-    if car_choise == "1":
-        print("u can choose a sedan")
-    else:
-        print("u chose an SUV")
-        
-else:
-    print("Invalid choise")
+choise = input("Enter +, -, *, or / : ")
+num1 = float(input("First number:"))
+num2 = float(input("Second number:"))
+
+if choise == "+":
+    add(num1,num2)
+if choise == "-":
+    sub(num1,num2)
+if choise == "*":
+    mul(num1,num2)
+if choise == "/":
+    div(num1,num2)
